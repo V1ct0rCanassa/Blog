f6baac44dd563850cb863fb606db7231b1ae9f7e
Vpn210 <victor.canassa@aluno.ifsp.edu.br>
2024-10-03 11:41:06 -0300

    dwad

--- post_formulario.php
+++ post_formulario.php
@@ -24,25 +24,53 @@
                 </div>
 
                 <div class="col-md-10" style="padding-top: 50px;">
+                    <?php 
                     
-                    <div class="card-header">Login</div>
-                    <div class="card-body">
-                        <form action="core/usuario_repositorio.php" method="post">
-                            <input type="hidden" name="acao" value="login">
+                        require_once 'includes/funcoes.php';
+                        require_once 'core/conexao_mysql.php';
+                        require_once 'core/sql.php';
+                        require_once 'core/mysql.php';
+
+                        foreach($_GET as $indice => $dado){
+                            $$indice = limparDados($dado);
+
+                        }
+                        if(!empty($id)){
+                            $id = (int)$id;
+
+                            $criterio = [
+                                ['id', '=', $id]
+                            ];
+
+                            $retorno = buscar(
+                                'post',
+                                ['*'],
+                                $criterio
+                            );
+                            $entidade = $retorno[0];
+                        }
+                    ?>
+                     
+                    <h2>Post</h2>
+                        <form action="core/post_repositorio.php" method="post">
+                            <input type="hidden" name="acao" value="<?php echo empty($id) ? 'insert' : 'update' ?>">
                         
+                            <input type="hidden" name="id" value="<?php echo $entidade['id'] ?? '' ?>">
+
                             <div class="form-group">
-                                <label for="email">E-mail</label>
-                                <input type="text" class="form-control" require='required'  id="email" name="email">
+                                <label for="titulo">Título</label>
+                                <input type="text" class="form-control" require='required'  id="titulo" name="titulo" value="<?php echo $entidade['titulo'] ?? '' ?>">
                             </div>
+
                             <div class="form-group">
-                                <label for="senha">Senha</label>
-                                <input type="password" class="form-control" require='required'  id="senha" name="senha">
+                                <label for="texto">Texto</label>
+                                <textarea name="texto" id="texto" require="required" rows="5" <?php echo $entidade['texto'] ?? '' ?>></textarea>
                             </div>
+                            
                             <div class="text-right">
                                 <button class="btn btn-success" type="submit">Acessar</button>
                             </div>
                         </form>
-                    </div>
                 </div>
             </div>
             <div class="row">
